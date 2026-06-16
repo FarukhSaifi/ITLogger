fc77df9c87b0bec0711ccc46fa9c0bc08ffe8c7e
Farukh Saifi <farook1x95@gmail.com>
2026-06-17 01:14:42 +0530

    Refactor React rendering and streamline Redux store setup
    - Updated ReactDOM rendering to use createRoot for improved performance.
    - Simplified Redux store creation by consolidating parameters into a single line.
    - Cleaned up import statements in logReducer for better readability.
    
    These changes enhance the overall structure and efficiency of the application.

--- src/store.js
+++ src/store.js
@@ -1,16 +1,12 @@
-import { createStore, applyMiddleware } from "redux";
-import { thunk } from "redux-thunk";
+import { applyMiddleware, createStore } from "redux";
 import { composeWithDevTools } from "redux-devtools-extension";
+import { thunk } from "redux-thunk";
 import rootReducer from "./reducer";
 
 const initialState = {};
 
 const middleware = [thunk];
 
-const store = createStore(
-  rootReducer,
-  initialState,
-  composeWithDevTools(applyMiddleware(...middleware))
-);
+const store = createStore(rootReducer, initialState, composeWithDevTools(applyMiddleware(...middleware)));
 
 export default store;
